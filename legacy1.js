e05ae753a1d01d5fc3ffcc107ba1f2fd8c576102
wrjeadew <154653458+wrjeadew@users.noreply.github.com>
2025-07-14 06:09:56 +0000

    Update legacy1.js

--- legacy1.js
+++ legacy1.js
@@ -1,9 +1,5 @@
-let legacyBasicInterval = setInterval(() => {
+let customBasicInterval = setInterval(() => {
     if (typeof(BAWK) === "undefined" || !BAWK.sounds || !Object.keys(BAWK.sounds)[0] || !BAWK.sounds[Object.keys(BAWK.sounds)[0]].buffer) return;
-    clearInterval(legacyBasicInterval);
+    clearInterval(customBasicInterval);
     BAWK.load("https://cdn.jsdelivr.net/gh/InfiniteSmasher/Legacy-Mode@latest/sounds1.json");
-    extern.catalog.findItemsByIds([3000, 3100, 3400, 3600, 3800, 4000, 4200]).forEach(item => {
-        item.name = item.name.replace(" ", " Legacy ");
-        item.item_data.meshName += "_Legacy";
-    });
 }, 250);
